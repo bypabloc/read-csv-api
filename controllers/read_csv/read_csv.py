b03d448f3e110ef006756ab8952a8e6eafa72a4b
Pablo <pacg1991@gmail.com>
2022-06-27 15:37:02 -0500

    Array validate last irem

--- controllers/read_csv/read_csv.py
+++ controllers/read_csv/read_csv.py
@@ -9,20 +9,18 @@ from app import request
 
 def is_float(value) -> bool or float:
     if value is None:
-        return False
+        return float(0)
     if value == '':
-        return False
+        return float(0)
     if value == 'NaN':
-        return False
+        return float(0)
     try:
         return float(value)
     except ValueError:
-        return False
+        return float(0)
 
 
 def read_csv():
-    print("read_csv")
-
     body = request.get_json()
     csv = body.get('csv')
     if csv is None:
@@ -40,39 +38,47 @@ def read_csv():
 
     csv_base64_content = csv_base64_content[1]
     decoded = base64.standard_b64decode(csv_base64_content).decode('latin-1')
-    spreadSheet = csv_lib.reader(decoded.splitlines(), delimiter=',', quotechar='"')
+    spreadSheet = csv_lib.reader(decoded.splitlines(), delimiter=',')
     data = []
     for (i, row) in enumerate(spreadSheet):
         if i == 0:
-            row_new = row[0].replace('Date,Time,"', '').split('","')
-            row_new[-1] = row_new[-1].replace('",', '')
-            data.append(row_new)
+            row_new = []
+            if len(row) == 1:
+                row_new = row[0].replace('Date,Time,"', '').split('","')
+                row_new[-1] = row_new[-1].replace('",', '')
+                data.append(row_new)
+            else:
+                data.append(row[2:-1])
             continue
         data.append(row)
+        if i == 50:
+            pass
 
     headers = data[0]
-    data = data[1:]
+    data = data[1:-2]
+    datetime_list = []
+    value_list = []
+    for i in range(0, len(data)):
+        item = data[i]
+        datetime_string = item[0] + " " + item[1]
+        datetime_string = datetime_string.replace('.', '/', 2)
+        datetime_obj = datetime.strptime(datetime_string, '%d/%m/%Y %H:%M:%S.%f')
+        datetime_string = datetime_obj.strftime('%Y-%m-%d %H:%M:%S')
+        datetime_list.append(datetime_string)
+        value_list.append(item[2:])
+
     data_for_graphs = {}
     for i in range(0, len(headers)):
-        data_for_graphs[headers[i]] = {
-            'values': [],
-            'datetime': [],
-        }
+        header = headers[i]
+        data_for_graphs[header] = []
         for j in range(0, len(data)):
-            value = is_float(data[j][i+2])
-            if value:
-                datetime_string = data[j][0] + " " + data[j][1]
-                datetime_string = datetime_string.replace('.', '/', 2)
-                datetime_obj = datetime.strptime(datetime_string, '%d/%m/%Y %H:%M:%S.%f')
-                datetime_string = datetime_obj.strftime('%Y-%m-%d %H:%M:%S')
-
-                data_for_graphs[headers[i]]['datetime'].append(datetime_string)
-                data_for_graphs[headers[i]]['values'].append(value)
+            data_for_graphs[header].append(is_float(data[j][i+2]))
 
     response = app.response_class(
         response=json.dumps({
             'data': {
                 'data_for_graphs': data_for_graphs,
+                'datetime_list': datetime_list,
             },
             'message': 'Csv readed',
         }),
